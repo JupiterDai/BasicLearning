a4ef604f662163dc6dc18780ebe62acd7b8b5442
JupiterDai <13050@192.168.35.1>
2020-10-19 15:36:50 +0800

    update

---
+++ src/ProcessControl/DemoIf.java
@@ -0,0 +1,19 @@
+package ProcessControl;
+
+import  java.util.*;
+
+public class DemoIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		if(a=="0") {
+			System.out.println("erro");
+		}else {
+			System.out.println("OK");
+		}
+	}
+
+
+}
