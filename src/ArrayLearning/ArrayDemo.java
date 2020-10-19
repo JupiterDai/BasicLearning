cf119588b878c3bdcd41e1af0b4e9946c1f16bd9
JupiterDai <13050@192.168.35.1>
2020-10-19 11:44:38 +0800

    update

---
+++ src/ArrayLearning/ArrayDemo.java
@@ -0,0 +1,15 @@
+package ArrayLearning;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int data[] = new int[2];
+		data[1] = 10;
+		data[0] = 4;
+		for (int i : data) {
+			System.out.println(i);
+		}
+	}
+
+}
